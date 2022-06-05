c44fc984f5e7558d6169acfe7f4a9c4f689a4763
Weverson-Luan <weverson.luan@smartinnovation.com.br>
2022-06-05 15:17:05 -0300

    config: created sentry configuration for error handling in our application.

--- src/shared/infra/http/app.ts
+++ src/shared/infra/http/app.ts
@@ -1,7 +1,9 @@
 import "reflect-metadata";
 import "dotenv/config"; 
-import express, { NextFunction, Request,Response, } from "express";
 import "express-async-errors";
+import express, { NextFunction, Request,Response, } from "express";
+import * as Sentry from "@sentry/node";
+import * as Tracing from "@sentry/tracing";
 import swaggerUi from "swagger-ui-express";
 import swaggerJson from "../../../../swagger.json";
 import upload from "@config/upload";
@@ -11,21 +13,34 @@ import "@shared/container";
 
 import { AppError } from "@shared/errors/AppError";
 
+import rateLimiter from "@shared/infra/http/middlewares/rateLimiter";
+
 import { router } from "@shared/infra/http/routes/index.routes";
 
 
 createConnection(); 
 const app = express();
+app.use(rateLimiter);
 
+Sentry.init({
+  dsn: process.env.SENTRY_DSN,
+  integrations: [
+    new Sentry.Integrations.Http({ tracing: true }),
+    new Tracing.Integrations.Express({ app }),
+  ],
+  tracesSampleRate: 1.0,
+});
+
+app.use(Sentry.Handlers.requestHandler());
+app.use(Sentry.Handlers.tracingHandler());
 app.use(express.json()); 
-app.use(
-  "/api/v1/rent-cars-docs",
-  swaggerUi.serve,
-  swaggerUi.setup(swaggerJson)
-);
+
+app.use("/api/v1/doc-rentx",swaggerUi.serve, swaggerUi.setup(swaggerJson));
 app.use("/avatar", express.static(`${upload.tmpFolder}/avatar`));
 app.use("/cars", express.static(`${upload.tmpFolder}/cars`));
 app.use(router);
+
+app.use(Sentry.Handlers.errorHandler());
 
 //middlewares de error
 app.use((error: Error, request:Request, response: Response, next: NextFunction)=> {
